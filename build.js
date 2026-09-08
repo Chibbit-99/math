aeea272bbc7b45a714ec39e73f93ff6cb7839702
Chibbit99 <chibbit99@gmail.com>
2026-09-08 17:32:22 +1000

    Load components script dynamically
    Added dynamic loading of components script before main script.

--- build.js
+++ build.js
@@ -5,7 +5,13 @@ fetch('/build/ui.html')
     document.write(html);
     document.close();
 
-    // Dynamically create and load the script
+    // Add components
+    const components = document.createElement('script');
+    script.src = '/scripts/components.js';
+    script.type = 'text/javascript';
+    document.body.appendChild(components);
+
+    // Run main script
     const script = document.createElement('script');
     script.src = '/scripts/main.js';
     script.type = 'text/javascript';
